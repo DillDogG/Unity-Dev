b3f2b94d54c9ad7fedcc201e3f251cfa0b3a4bbc
DillDogG <dilldoggaming@gmail.com>
2024-01-09 13:38:56 -0700

    Adding movements

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public Transform startPoint;
+    public Transform endPoint;
+    private Rigidbody rb;
+    private bool toEnd = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toEnd){
+            transform.LookAt(endPoint);
+            rb.MovePosition(rb.position + endPoint.position * Time.fixedDeltaTime);
+            if (transform.position.x <= endPoint.position.x + 1 && transform.position.z <= endPoint.position.z + 1) {
+                if (transform.position.x >= endPoint.position.x - 1 && transform.position.z >= endPoint.position.z - 1) toEnd = false;
+            }
+        }
+        else {
+            transform.LookAt(startPoint);
+            rb.MovePosition(rb.position + startPoint.position * Time.fixedDeltaTime);
+            if (transform.position.x <= startPoint.position.x + 1 && transform.position.z <= startPoint.position.z + 1) {
+                if (transform.position.x >= startPoint.position.x - 1 && transform.position.z >= startPoint.position.z - 1) toEnd = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * moveSpeed;
+
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+    }
+}
