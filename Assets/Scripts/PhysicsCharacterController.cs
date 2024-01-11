7e9df5c6080eb81fcd5c2a252b6a6d134d70dbf4
DillDogG <dilldoggaming@gmail.com>
2024-01-11 14:30:03 -0700

    Update

--- Assets/Scripts/PhysicsCharacterController.cs
+++ Assets/Scripts/PhysicsCharacterController.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PhysicsCharacterController : MonoBehaviour
 {
-    [SerializeField][Range(1, 10)] float maxForce;
-    [SerializeField][Range(1, 10)] float jumpForce;
+    [Header("Movement")]
+    [SerializeField][Range(1, 10)] float maxForce = 5;
+    [SerializeField][Range(1, 10)] float jumpForce = 5;
     [SerializeField] Transform view;
+    [Header("Collision")]
+    [SerializeField][Range(0, 5)] float rayLength = 1;
+    [SerializeField] LayerMask groundLayerMask;
     Rigidbody rb;
     Vector3 force = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -24,15 +29,23 @@ public class PhysicsCharacterController : MonoBehaviour
         direction.x = Input.GetAxis("Horizontal");
         direction.z = Input.GetAxis("Vertical");
 
-        force = view.rotation * direction * maxForce;
-        if (Input.GetButtonDown("Jump"))
+        Quaternion yrotation = Quaternion.AngleAxis(view.rotation.eulerAngles.y, Vector3.up);
+
+        force = yrotation * direction * maxForce;
+        Debug.DrawRay(transform.position, Vector3.down * rayLength, Color.red);
+        if (Input.GetButtonDown("Jump") && CheckGround())
         {
-            rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 
     private void FixedUpdate()
     {
         rb.AddForce(force, ForceMode.Force);
+    }
+
+    private bool CheckGround()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, rayLength, groundLayerMask);
     }
 }
