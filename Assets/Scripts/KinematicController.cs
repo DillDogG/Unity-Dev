c7fcf8c1d90759bc2ea38c8bb7f5b1fa7a803eca
DillDogG <dilldoggaming@gmail.com>
2024-02-05 13:47:26 -0700

    update

--- Assets/Scripts/KinematicController.cs
+++ Assets/Scripts/KinematicController.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KinematicController : MonoBehaviour
+public class KinematicController : MonoBehaviour, IDamagable
 {
     [SerializeField, Range(0, 40)] float speed = 1;
     [SerializeField] float maxDistance = 5;
+
+    public float health = 100;
+
+    public void ApplyDamage(float damage)
+    {
+        health -= damage;
+    }
+
     void Update()
     {
         Vector3 direction = Vector3.zero;
