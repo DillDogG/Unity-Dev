ca76f76424ef975a90c0514fef45588df0fa17f9
DillDogG <dilldoggaming@gmail.com>
2024-02-06 15:22:17 -0700

    Class 2/6

---
+++ Assets/Scripts/Items/ProjectileAmmo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileAmmo : Ammo
+{
+    [SerializeField] Action action;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (action != null)
+        {
+            action.onEnter = OnInteractStart;
+            action.onStay = OnInteractActive;
+        }
+
+        if (ammoData.force != 0) GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * ammoData.force, ammoData.forceMode);
+    }
+
+}
