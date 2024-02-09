fe641bf679407f794b7ad2801d6a7ff7dc8359f9
DillDogG <dilldoggaming@gmail.com>
2024-02-08 17:47:14 -0700

    Added player

---
+++ Assets/Scripts/Items/Inventory.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : Item
+{
+    public override ItemData GetData()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool isReady()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnAnimEventItemUse()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void StopUse()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Use()
+    {
+        throw new System.NotImplementedException();
+    }
+}
--- Assets/Scripts/KinematicController.cs
+++ Assets/Scripts/KinematicController.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KinematicController : MonoBehaviour, IDamagable
+public class KinematicController : MonoBehaviour//, IDamagable
 {
     [SerializeField, Range(0, 40)] float speed = 1;
     [SerializeField] float maxDistance = 5;
 
-    public float health = 100;
+    //public float health = 100;
 
-    public void ApplyDamage(float damage)
-    {
-        health -= damage;
-    }
+    //public void ApplyDamage(float damage)
+    //{
+    //    health -= damage;
+    //}
 
     void Update()
     {
@@ -24,5 +24,11 @@ public class KinematicController : MonoBehaviour, IDamagable
         Vector3 force = direction * speed * Time.deltaTime;
         transform.localPosition += force;
         transform.localPosition = Vector3.ClampMagnitude(transform.localPosition, maxDistance);
+
+        Quaternion qyaw = Quaternion.AngleAxis(direction.x * 10, Vector3.up);
+        Quaternion qpitch = Quaternion.AngleAxis(-direction.y * 10, Vector3.right);
+
+        Quaternion rotation = qyaw * qpitch;
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, rotation, 10 * Time.deltaTime);
     }
 }
